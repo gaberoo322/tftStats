e76eebf45f97461b6fa18e586ece64079453df6b
Graham Greenfield <gram@Grahams-MacBook-Pro.local>
2020-05-23 15:40:44 -0700

    button time

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <button>Graham</button>
+    <button>Gabe</button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
