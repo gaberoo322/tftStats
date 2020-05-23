27c1a04765735e2f0b351a30b0531a85b75f5181
Gabe Greenfield <g.greenfield@aspenware.com>
2020-05-23 16:28:29 -0600

    changing the title of our app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Team Fight Tactics Statistics`,
+    description: `TFT STATS DUDE`,
+    author: `@GabeAndGraham`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
